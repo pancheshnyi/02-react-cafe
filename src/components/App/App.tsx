1d6411ef8b8c6ed11af2ff6da4436126bf9cb4ac
Oleg_Pancheshniy <oleg.pancheshniy@gmail.com>
2025-09-01 20:30:49 +0300

    fix bug vercel

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
-import { Votes, VoteType } from "../../types/votes";
+import type { Votes, VoteType } from "../../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
